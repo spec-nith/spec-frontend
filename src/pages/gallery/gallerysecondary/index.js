5b82fa9315364d47462418edbe6e288ef4721b15
priyanshu-rajput-01 <priyanshurajput0071109@gmail.com>
2021-10-01 13:38:03 +0530

    add routes for inernal gallery

---
+++ src/pages/gallery/gallerysecondary/index.js
@@ -0,0 +1,37 @@
+import React, { useState,Component } from 'react';
+
+import '../../../assets/styles/gallery.css';
+
+export default class GallerySecondary extends Component {
+    constructor(props){
+        
+        super(props);
+        this.state = {
+            title : props.location.search.slice(6),
+            data : []
+
+        };
+    }
+    
+    SecondaryComponent = this.state.data.map((card)=>{
+        return (
+            
+            <div className="gallery-panel" style={{ backgroundImage: card.img }}>
+                <h3>{card.year}</h3>
+            </div>
+        )
+    })
+    render() {
+        return (
+            <div className= "gallery-color-scheme">
+                <h1>{this.state.title}</h1>
+            <div className="container-int">
+                
+                <div className="container">
+                    {this.SecondaryComponent}
+                </div>
+            </div>
+            </div>
+        )
+    }
+}
--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import '../../assets/styles/gallery.css';
+import axios from 'axios';
 
-
-// import { Link } from 'react-router-dom';
+import { Link } from 'gatsby';
 
 let galleryCards = [{
     title: "SPEC",
@@ -34,25 +34,36 @@ export default class Gallery extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            data : [],
         };
     }
-
+    componentDidMount() {
+        console.log("Mount running");
+        axios.get('https://spec-backend.herokuapp.com/api/gallery/')
+          .then(response => {
+            console.log(response.data)
+            this.setState({ data: response.data });
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      }
     GalleryComponent = galleryCards.map((props) => {
         return (
-            <div className="internal-con">
+            <div className="internal-con ">
                 <div className="background" style={{ backgroundImage: props.img }}></div>
                 <div className="text">
                     <div class="gallery-card-title-medium">{props.title}</div>
                     <div className="gallery-card-title">{props.sub}</div>
                     <div className="gallery-card-title-small">Gallery collection</div>
-                    <button className="btn-prime">Gallary</button>
+                    <button className="btn-prime"><Link to = {`/gallery/gallerysecondary/?name=${props.sub}`}>Gallary</Link></button>
                 </div>
             </div>
         )
     });
     render() {
         return (
-            <div className='gallery-main'>
+            <div className='gallery-main gallery-color-scheme'>
                 <div className="main-container">
                     {this.GalleryComponent}
                 </div>
