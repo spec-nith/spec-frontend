5027349f148ead263d1516ee2f844cbcbcf4503e
priyanshu-rajput-01 <priyanshurajput0071109@gmail.com>
2021-11-09 18:24:34 +0530

    added

--- src/pages/gallery/view/index.js
+++ src/pages/gallery/view/index.js
@@ -1,92 +1,120 @@
-import React, { Component } from "react";
-import Gallery from "react-photo-gallery";
-import { photos } from "./photos";
+import React, { Component , useState, useCallback } from "react";
 import Layout from "components/UI/Layout/Layout";
 import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlusCircle,
-  faChevronRight,
-  faChevronLeft,
-  faTimes,
-} from "@fortawesome/free-solid-svg-icons";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "./photos";
 import "../../../assets/styles/gallery.css";
-export default class Images extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          FullImageCard: false,
-          imageUrl: null,
-          currentIndex: 0,
-          data: [],
-          title: this.props.location.search.slice(6),
+// export default class Images extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//           FullImageCard: false,
+//           imageUrl: null,
+//           currentIndex: 0,
+//           data: [],
+//           title: this.props.location.search.slice(6),
     
-        };
-      }
-      async componentDidMount() {
-        await axios
-          .get("https://spec-backend.herokuapp.com/api/gallery/?event=" + this.state.title)
-          .then((response) => {
-                let rs = response.data;
-            for(let i =0;i< response.data.length;i++) {
-                rs[i].src = response.data[i].thumb_image_url;
-                rs[i].width =3;
-                rs[i].height = 4;
-                if(i%3===0){
-                    rs[i].width = 4;
-                    rs[i].height = 3;
-                }
-                if(i%2===1){
-                  rs[i].width = 3;
-                  rs[i].height = 2;
-                }
-                else if(i%2===0){
-                  rs[i].width = 4;
-                  rs[i].height = 3;
-                }
-                if(i%7===0){
-                    rs[i].width = 3;
-                    rs[i].height = 4;
-                  }
-                if(i%5===0){
-                    rs[i].width = 6;
-                    rs[i].height = 5;
-                }
-                if(i%4===0){
-                  rs[i].width = 4;
-                  rs[i].height = 2;
-                }
-            }
-            console.log(rs);
-            this.setState({ data: rs });
+//         };
+//       }
+//       async componentDidMount() {
+//         await axios
+//           .get("https://spec-backend.herokuapp.com/api/gallery/?event=" + this.state.title)
+//           .then((response) => {
+//                 let rs = response.data;
+//             for(let i =0;i< response.data.length;i++) {
+//                 rs[i].src = response.data[i].thumb_image_url;
+//                 rs[i].width =3;
+//                 rs[i].height = 4;
+//                 if(i%3===0){
+//                     rs[i].width = 4;
+//                     rs[i].height = 3;
+//                 }
+//                 if(i%2===1){
+//                   rs[i].width = 3;
+//                   rs[i].height = 2;
+//                 }
+//                 else if(i%2===0){
+//                   rs[i].width = 4;
+//                   rs[i].height = 3;
+//                 }
+//                 if(i%7===0){
+//                     rs[i].width = 3;
+//                     rs[i].height = 4;
+//                   }
+//                 if(i%5===0){
+//                     rs[i].width = 6;
+//                     rs[i].height = 5;
+//                 }
+//                 if(i%4===0){
+//                   rs[i].width = 4;
+//                   rs[i].height = 2;
+//                 }
+//             }
+//             console.log(rs);
+//             this.setState({ data: rs });
             
-            console.log('me',this.state.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
+            
+//           })
+//           .catch((err) => {
+//             console.log(err);
+//           });
+//       }
+       
+//     render() {
       
-      inLarge(event,{index,photo}){
-        console.log(index);
-        this.setState({
-          FullImageCard: true,
-          imageUrl: photo.image_url,
-          currentIndex: index,
-        });
-      }
-    render() {
-        return (
-            <Layout>
-          <h1 className="text-5xl font-bold text-center mt-16 sm:text-7xl md:text-8xl">{this.state.title}</h1>
+//         return (
+//             <Layout>
+//           <h1 className="text-5xl font-bold text-center mt-16 sm:text-7xl md:text-8xl">{this.state.title}</h1>
           
-                <div className="mx-32 my-16">
+//                 <div className="mx-32 my-16">
 
-                    <Gallery photos={photos} direction={"row"} />
+//                     <Gallery photos={photos} direction={"row"} onClick={this.state.inLarge} />
 
-                </div>
-            </Layout>
-        );
-    }
+//                 </div>
+//             </Layout>
+//         );
+//     }
+// }
+
+
+export default function ImageGal() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  return (
+    <Layout>
+        <h1 className="text-5xl font-bold text-center mt-16 sm:text-7xl md:text-8xl">something</h1>
+    <div>
+      
+      <Gallery photos={photos} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+    </Layout>
+  );
 }
+
 
--- src/pages/gallery/view/photos.js
+++ src/pages/gallery/view/photos.js
@@ -1,46 +1,46 @@
 export const photos = [
   {
-    src: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
     width: 4,
     height: 3
   },
   {
-    src: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
     width: 1,
     height: 1
   },
   {
-    src: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
     width: 3,
     height: 4
   },
   {
-    src: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
     width: 3,
     height: 4
   },
   {
-    src: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
     width: 3,
     height: 4
   },
   {
-    src: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
     width: 4,
     height: 3
   },
   {
-    src: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
     width: 3,
     height: 4
   },
   {
-    src: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
     width: 4,
     height: 3
   },
   {
-    src: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
     width: 4,
     height: 3
   }
