13b0e0582f066fc3cd0c62cd2bb58028aca2a4d9
PriyanshuRj <Priyanshurajput@0071109@gmail.com>
2022-01-27 21:54:37 +0530

    Updated Gallery view

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -3,7 +3,7 @@ import Layout from "components/UI/Layout/Layout";
 import { Link } from "gatsby";
 import "assets/styles/gallery.css";
 
-export default class GallerySecondary extends Component {
+export default class Gallery extends Component {
   constructor(props) {
     super(props);
     this.state = {};
@@ -11,63 +11,51 @@ export default class GallerySecondary extends Component {
 
   render() {
     return (
-      <Layout>
-        <h1 className="text-5xl font-bold text-center mt-16 sm:text-7xl md:text-8xl">
-          Gallery
-        </h1>
+      <Layout >
+
+          <h1 className="font-abrilface text-center text-5xl text-center mt-16 sm:text-7xl md:text-8xl">GALLERY</h1>
 
         <div className="mx-10 mt-28 xl:mx-28 grid gap-4 xl:gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 text-2xl font-bold xl:text-3xl">
-          <div
-            className="w-58 h-120 lg:h-120 lg:row-span-2 bg-gray-400 object-center gallery-main-boxes"
-            style={{ backgroundImage: `url('images/gallery/specfest.jpg')` }}
-          >
+          <div className="w-58 h-120 lg:h-120 lg:row-span-2 bg-gray-400 object-center gallery-main-boxes" style={{ backgroundImage: `url('images/gallery/specfest.jpg')` }}>
             <div className="gallay-mainpage-inner">
               <Link to="/gallery/view/?page=SpecFest">
                 <button className="text-2xl font-bold xl:text-3xl hover:bg-gray-100 hover:bg-opacity-20 text-white font-bold py-2 px-4 rounded">
-                  <h3>SPECFEST</h3>
+                <h3 className="font-abrilface">SPECFEST</h3>
                 </button>
               </Link>
             </div>
           </div>
 
-          <div
-            className="w-58 h-60 bg-gray-400 object-scale-down gallery-main-boxes"
-            style={{ backgroundImage: `url('images/gallery/electrothon.jpg')` }}
-          >
+          <div className="w-58 h-60 bg-gray-400 object-scale-down gallery-main-boxes" style={{ backgroundImage:  `url('images/gallery/electrothon.jpg')` }}>
             <div className="gallay-mainpage-inner">
               <Link to="/gallery/view/?page=Electrothon">
-                <button className="text-2xl font-bold xl:text-3xl hover:bg-gray-100 hover:bg-opacity-20 text-white font-bold py-2 px-4 rounded">
-                  <h3>ELECTROTHON</h3>
+              <button className="text-2xl font-bold xl:text-3xl hover:bg-gray-100 hover:bg-opacity-20 text-white font-bold py-2 px-4 rounded">
+                <h3 className="font-abrilface">ELECTROTHON</h3>
                 </button>
               </Link>
             </div>
           </div>
-          <div
-            className="w-58 h-60 lg:col-span-2 bg-gray-400 object-cover gallery-main-boxes"
-            style={{ backgroundImage: `url('images/gallery/workshop.jpg')` }}
-          >
+          <div className="w-58 h-60 lg:col-span-2 bg-gray-400 object-cover gallery-main-boxes" style={{ backgroundImage: `url('images/gallery/workshop.jpg')` }}>
             <div className="gallay-mainpage-inner">
               <Link to="/gallery/view/?page=Workshop">
-                <button className="text-2xl font-bold xl:text-3xl hover:bg-gray-100 hover:bg-opacity-20 text-white font-bold py-2 px-4 rounded">
-                  <h3>WORKSHOP</h3>
+              <button className="text-2xl font-bold xl:text-3xl hover:bg-gray-100 hover:bg-opacity-20 text-white font-bold py-2 px-4 rounded">
+                <h3 className="font-abrilface">WORKSHOP</h3>
                 </button>
               </Link>
             </div>
           </div>
-          <div
-            className="w-58 h-60 lg:col-span-3 bg-gray-400  object-cover gallery-main-boxes"
-            style={{ backgroundImage: `url('images/gallery/random.jpg')` }}
-          >
+          <div className="w-58 h-60 lg:col-span-3 bg-gray-400  object-cover gallery-main-boxes" style={{ backgroundImage: `url('images/gallery/random.jpg')` }}>
             <div className="gallay-mainpage-inner">
               <Link to="/gallery/view/?page=Random">
-                <button className="text-2xl font-bold xl:text-3xl hover:bg-gray-100 hover:bg-opacity-20 text-white font-bold py-2 px-4 rounded">
-                  <h3>RANDOM</h3>
+              <button className="text-2xl font-bold xl:text-3xl hover:bg-gray-100 hover:bg-opacity-20 text-white font-bold py-2 px-4 rounded">
+                <h3 className="font-abrilface">RANDOM</h3>
                 </button>
               </Link>
             </div>
           </div>
           <div className="mt-10"></div>
         </div>
+        
       </Layout>
     );
   }
--- src/pages/gallery/view/index.js
+++ src/pages/gallery/view/index.js
@@ -1,153 +1,129 @@
-// Import Components
 import React, { Component } from "react";
 import Layout from "components/UI/Layout/Layout";
 import axios from "axios";
-// import Gallery from "react-photo-gallery";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { galleryURL } from "../../../components/Routes";
-// import {
-//   faChevronRight,
-//   faChevronLeft,
-//   faTimes,
-// } from "@fortawesome/free-solid-svg-icons";
-import Gallery from "components/UI/Gallery/Gallery";
-// import LightGallery from 'lightgallery/react';
-// import lgThumbnail from 'lightgallery/plugins/thumbnail';
-// import lgZoom from 'lightgallery/plugins/zoom';
-
-//CSS
-// import 'lightgallery/css/lightgallery.css';
-// import 'lightgallery/css/lg-zoom.css';
-// import 'lightgallery/css/lg-thumbnail.css';
+import Gallery from "react-photo-gallery";
+import { photos } from "./photos";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {galleryURL} from "../../../components/Routes";
+import {
+  faChevronRight,
+  faChevronLeft,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 import "assets/styles/gallery.css";
-
-// Constants
-// const photos = require("./photos.json");
-
-// console.log(lightgallery)
-// console.log(gallery)
 export default class Images extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      FullImageCard: false,
-      imageUrl: null,
-      currentIndex: 0,
-      data: [],
-      title: this.props.location.search.slice(6),
-      // tt: this.props.location,
-      currentImage: 0,
-      viewerIsOpen: false,
-      load_status: true,
-    };
-  }
-  componentDidMount() {
-    let url = "/?event=" + this.state.title;
-    // console.log(this.state.tt)
-    if (this.state.title === "Random") {
-      url = "";
+    constructor(props) {
+        super(props);
+        this.state = {
+            FullImageCard: false,
+            imageUrl: null,
+            currentIndex: 0,
+            data: [],
+            title: this.props.location.search.slice(6),
+            currentImage: 0,
+            viewerIsOpen: false
+        };
+        
     }
-    console.log(this.state.url);
-    axios
-      .get(galleryURL + url)
-      .then((response) => {
-        let photos = response.data;
-        // function select(a,b){
-        //     if(a.year>b.year){
-        //         return 1;
-        //     }
-        //     else{
-        //         return -1;
-        //     }
-        // }
-        // rs.sort(select);
-        // console.log(rs);
-        // const photos = rs;
-        // for (let i = 0; i < response.data.length; i++) {
-        //     rs[i].src = response.data[i].thumb_image_url;
-        //     rs[i].width = 3;
-        //     rs[i].height = 4;
-        //     if (i % 3 === 0) {
-        //         rs[i].width = 4;
-        //         rs[i].height = 3;
-        //     }
-        //     if (i % 2 === 1) {
-        //         rs[i].width = 3;
-        //         rs[i].height = 2;
-        //     }
-        //     else if (i % 2 === 0) {
-        //         rs[i].width = 4;
-        //         rs[i].height = 3;
-        //     }
-        //     if (i % 7 === 0) {
-        //         rs[i].width = 3;
-        //         rs[i].height = 4;
-        //     }
-        //     if (i % 5 === 0) {
-        //         rs[i].width = 6;
-        //         rs[i].height = 5;
-        //     }
-        //     if (i % 4 === 0) {
-        //         rs[i].width = 4;
-        //         rs[i].height = 2;
-        //     }
-        // }
-        this.setState({ data: photos });
-        this.setState({ load_status: false });
-        console.log(photos);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  // openLightbox = (event, { photo, index }) => {
-  //     this.setState({ currentImage: index, viewerIsOpen: true })
-  //     this.showImage(photo,index);
-  // };
-  // closeLightbox = () => {
-  //     this.setState({ currentImage: 0, viewerIsOpen: false })
-  // };
-  // showImage = (img, ind) => {
-  //     let url = this.state.data[ind].image_url;
-  //     this.setState({
-  //         FullImageCard: true,
-  //         imageUrl: url,
-  //         currentIndex: ind,
-  //     });
-  // }
-  // showPrev = (ind) => {
-  //     if (ind > 0) {
-  //         let url = this.state.data[ind - 1].image_url;
-  //         this.setState({
-  //             FullImageCard: true,
-  //             imageUrl: url,
-  //             currentIndex: ind - 1,
-  //         });
-  //     }
-  // }
-  // showNext = (ind) => {
-  //     if (ind < this.state.data.length - 1) {
-  //         let url = this.state.data[ind + 1].image_url;
-  //         this.setState({
-  //             FullImageCard: true,
-  //             imageUrl: url,
-  //             currentIndex: ind + 1,
-  //         });
-  //     }
-  // }
-  // exitButton = () => {
-  //     this.setState({ FullImageCard: false });
-  // }
-  onInit = () => {
-    console.log("LG init");
-  };
-  render() {
-    return (
-      <Layout>
-        {/* <React.Fragment> */}
-
-        {/* <h1 className="text-5xl font-bold text-center mt-16 sm:text-7xl md:text-8xl">{this.state.title}</h1> */}
-        {/* {this.state.FullImageCard && (
+    async componentDidMount() {
+        let url = "/?event=" + this.state.title;
+        if(this.state.title ==="Random"){
+            url = "";
+        }
+        console.log(this.state.url);
+        await axios
+            .get(galleryURL + url)
+            .then((response) => {
+                let rs = response.data;
+                function select(a,b){
+                    if(a.year>b.year){
+                        return 1;
+                    }
+                    else{
+                        return -1;
+                    }
+                }
+                rs.sort(select);
+                console.log(rs);
+                for (let i = 0; i < response.data.length; i++) {
+                    rs[i].src = response.data[i].thumb_image_url;
+                    rs[i].width = 3;
+                    rs[i].height = 4;
+                    if (i % 3 === 0) {
+                        rs[i].width = 4;
+                        rs[i].height = 3;
+                    }
+                    if (i % 2 === 1) {
+                        rs[i].width = 3;
+                        rs[i].height = 2;
+                    }
+                    else if (i % 2 === 0) {
+                        rs[i].width = 4;
+                        rs[i].height = 3;
+                    }
+                    if (i % 7 === 0) {
+                        rs[i].width = 3;
+                        rs[i].height = 4;
+                    }
+                    if (i % 5 === 0) {
+                        rs[i].width = 6;
+                        rs[i].height = 5;
+                    }
+                    if (i % 4 === 0) {
+                        rs[i].width = 4;
+                        rs[i].height = 2;
+                    }
+                }
+                this.setState({ data: rs });
+                console.log(photos);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    openLightbox = (event, { photo, index }) => {
+        this.setState({ currentImage: index, viewerIsOpen: true })
+        this.showImage(photo,index);
+    };
+    closeLightbox = () => {
+        this.setState({ currentImage: 0, viewerIsOpen: false })
+    };
+    showImage = (img, ind) => {
+        let url = this.state.data[ind].image_url;
+        this.setState({
+            FullImageCard: true,
+            imageUrl: url,
+            currentIndex: ind,
+        });
+    }
+    showPrev = (ind) => {
+        if (ind > 0) {
+            let url = this.state.data[ind - 1].image_url;
+            this.setState({
+                FullImageCard: true,
+                imageUrl: url,
+                currentIndex: ind - 1,
+            });
+        }
+    }
+    showNext = (ind) => {
+        if (ind < this.state.data.length - 1) {
+            let url = this.state.data[ind + 1].image_url;
+            this.setState({
+                FullImageCard: true,
+                imageUrl: url,
+                currentIndex: ind + 1,
+            });
+        }
+    }
+    exitButton = () => {
+        this.setState({ FullImageCard: false });
+    }
+    render() {
+        return (
+            <Layout>
+                <h1 className="text-5xl text-center mt-16 sm:text-7xl md:text-8xl font-abrilface">{this.state.title}</h1>
+                {this.state.FullImageCard && (
             <div id="overlay">
               <div
                 id="prevButton"
@@ -174,29 +150,15 @@ export default class Images extends Component {
                 <FontAwesomeIcon icon={faTimes} />
               </div>
             </div>
-          )} */}
-
-        {/* <div className="mx-8 lg:mx-16 2xl:mx-32 mt-16 pb-32"> */}
-        <Gallery photos={this.state.data} loadStatus={this.state.load_status} />
-        {/* <LightGallery
-                onInit={this.onInit}
-                speed={500}
-                plugins={[lgThumbnail, lgZoom]}
-            >
-                {
-                    this.state.data.map((img_data) => {
-                        return(
-                            <a data-src={img_data.image_url} key={img_data.id} className="gallery-item">
-                                <img src={img_data.thumb_image_url} className="img-responsive"/>
-                            </a>
-                        )
-                    })
-                }
-            </LightGallery> */}
-        {/* <div className="mt-10"></div> */}
-        {/* </div> */}
-        {/* </React.Fragment> */}
-      </Layout>
-    );
-  }
+          )}
+          
+                <div className="mx-8 lg:mx-16 2xl:mx-32 mt-16 pb-32">
+                    <Gallery photos={this.state.data} onClick={this.openLightbox} />
+                <div className="mt-10"></div>
+                </div>
+            </Layout>
+        );
+    }
 }
+
+
