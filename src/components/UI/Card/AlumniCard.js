f5c3289fd572009fada080d67769d8ec466a8701
Avishrant Sharma <avishrants@gmail.com>
2022-01-27 21:33:31 +0530

    Alumni Page Layout changed, font fixed. (#51) (#52)
    * Changed bg, about, swiper
    
    * Added particlejs, new project section
    
    * New domain, deploy command
    
    * Update CNAME config
    
    Signed-off-by: Avishrant Sharma <avishrants@gmail.com>
    
    * Added vector imgs, edit gatsby-config
    
    * Redesigned Workshop Page
    
    * edited vision section, colour changes
    
    * layout changed
    
    Co-authored-by: DragonEmperor <dragnemperor@gmail.com>
    Co-authored-by: JgtAman <41839176+DragnEmperor@users.noreply.github.com>
    Co-authored-by: Avishrant Sharma <avishrants@gmail.com>
    Co-authored-by: Abhay <leader.abhay002@gmail.com>
    
    Co-authored-by: Smriti <77229239+Smriti925@users.noreply.github.com>
    Co-authored-by: DragonEmperor <dragnemperor@gmail.com>
    Co-authored-by: JgtAman <41839176+DragnEmperor@users.noreply.github.com>
    Co-authored-by: Abhay <leader.abhay002@gmail.com>

--- src/components/UI/Card/AlumniCard.js
+++ src/components/UI/Card/AlumniCard.js
@@ -4,12 +4,12 @@ import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function spec({ person }) {
   return (
-    <div className="card">
-      <div>
-        <div className="card_bg rounded-2xl overflow-hidden shadow-lg w-72 h-96 border border-indigo-600">
+    <div className="card bg-opacity-100">
+      <div className="font-monty">
+        <div className="card_bg rounded-2xl overflow-hidden shadow-lg w-72 h-96">
           <img
             src={person.profile_pic_url}
-            // src="https://image.oppo.com/content/dam/oppo/product-asset-library/a/a93/v1/sec-4-img-3.jpg"
+            src="https://image.oppo.com/content/dam/oppo/product-asset-library/a/a93/v1/sec-4-img-3.jpg"
             // src="https://image.freepik.com/free-photo/handsome-young-man-with-new-stylish-haircut_176420-19637.jpg"
             className="w-80 h-72 object-fill"
             alt="img"
@@ -17,13 +17,13 @@ function spec({ person }) {
           <div className=" bottom_text px-4 pt-1 w-auto ">
             <div className="text-left text-md">
               <ul className="">
-                <div className="alumni_batch text-sm font-normal pb-2">
+                <div className="alumni_batch text-sm font-normal pb-2 text-gray-200">
                   {person.batch}
                 </div>
-                <li className="uppercase text-gray-200 font-bold">
+                <li className="uppercase text-white font-bold">
                   {person.name}
                 </li>
-                <li className="uppercase font-normal text-xs text-gray-400">
+                <li className="uppercase font-normal text-xs text-gray-200">
                   {person.company}
                 </li>
                 <a
--- src/pages/alumni/index.js
+++ src/pages/alumni/index.js
@@ -6,11 +6,15 @@ import Layout from "components/UI/Layout/Layout";
 import { alumniURL } from "components/Routes";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import Loader from "react-loader-spinner";
 
 class Alumni extends Component {
   constructor() {
     super();
     this.state = {
+      error: false,
+      wait: true,
+      data: [],
       selected_year: 0,
       dummy: [],
     };
@@ -19,10 +23,14 @@ class Alumni extends Component {
     axios
       .get(alumniURL)
       .then((response) => {
-        this.setState({ dummy: response.data });
+        this.setState({
+          dummy: response.data,
+          wait: false
+        });
       })
       .catch((err) => {
         console.log(err);
+        this.setState({ error: true })
       });
   }
   Selected_Year = (e) => {
@@ -44,130 +52,155 @@ class Alumni extends Component {
     const year_of_grad_options = [2021, 2020, 2019, 2018, "Before 2018"];
     const before_18 = [2017, 2016, 2015];
     var year_blank = [];
-    return (
-      <Layout>
-        <div className="alumni_bg px-auto">
-          <header className="head">
-            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-              <div className="our_alumni text-6xl font-bold text-white text-center">
-                OUR ALUMNI
-              </div>
-            </div>
-          </header>
-          <div className="flex justify-end">
-            <span className="mt-20 mr-2">
-              {" "}
-              <FontAwesomeIcon icon={faFilter} size="1x" />
-            </span>
-            <select className="years h-9" onChange={this.Selected_Year}>
-              <option value={0}>All Year </option>
-              {year_of_grad_options.map((obj) => (
-                <option value={obj}>{obj}</option>
-              ))}
-            </select>
+
+    if (this.state.wait) {
+      return (
+        <Layout>
+          <div className="flex h-90v justify-center items-center">
+            <Loader
+              type="Puff"
+              color="#00BFFF"
+              height={100}
+              width={100}
+              timeout={100000} // 10 secs wait until error message shows
+            />
           </div>
-          <div
-            className={
-              this.state.selected_year == 0
-                ? "flex flex-col"
-                : "flex justify-center"
-            }
-            style={{ padding: "auto" }}
-          >
-            {year_of_grad_options.map((obj) => (
-              <React.Fragment key={obj}>
-                {this.state.selected_year == 0 ? (
-                  <>
-                    <div className="batch">{"Batch " + obj}</div>
+        </Layout>
+
+      )
+    }
+    else {
+      return (
+
+        <Layout>
+          <div className="alumni_bg px-auto">
+            <header className="head">
+              <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div className="our_alumni text-6xl font-outfit text-white text-center">
+                  OUR ALUMNI
+                </div>
+              </div>
+            </header>
+            <div className="flex mt-5 mr-20 justify-end">
+              <span className="mt-14 ml-2 mr-2 ">
+                <FontAwesomeIcon icon={faFilter} size="1x" />
+              </span>
+              <select
+                className="h-10 text-white bg-zinc-800  rounded-lg p-2 font-monty "
+                onChange={this.Selected_Year}
+              >
+                <option value={0}>All Year</option>
+                {year_of_grad_options.map((obj) => (
+                  <option value={obj}>{obj}</option>
+                ))}
+              </select>
+            </div>
+            <div
+              className={
+                this.state.selected_year == 0
+                  ? "flex flex-col"
+                  : "flex justify-center"
+              }
+              style={{ padding: "auto" }}
+            >
+              {year_of_grad_options.map((obj) => (
+                <React.Fragment key={obj}>
+                  {this.state.selected_year == 0 ? (
+                    <>
+                      <div className="batch text-white text-4xl font-monty">
+                        Batch{" "}
+                        <span style={{ color: "rgb(46, 224, 154)" }}>{obj}</span>
+                      </div>
+                      <div
+                        className="hide"
+                        id={obj}
+                        style={{ display: "flex", margin: "0 8vw 30px 8vw" }}
+                      >
+                        <section
+                          className="carousel_cards_container"
+                          style={checking}
+                        >
+                          <div
+                            className="card_block gap-4 mb-4 "
+                            style={{ display: "flex" }}
+                          >
+                            {this.state.dummy.map((test) =>
+                              test.batch === obj ? (
+                                <Cards person={test} key={test.id} />
+                              ) : obj == "Before 2018" && test.batch < 2018 ? (
+                                <Cards person={test} key={test.id} />
+                              ) : null
+                            )}
+                          </div>
+                        </section>
+                      </div>
+                    </>
+                  ) : obj != this.state.selected_year ? (
                     <div
                       className="hide"
                       id={obj}
-                      style={{ display: "flex", margin: "0 8vw 30px 8vw" }}
+                      style={{ display: "flex", margin: "0 8vw 10px 8vw" }}
+                      style={{ display: "none" }}
                     >
-                      <section
-                        className="carousel_cards_container"
-                        style={checking}
-                      >
-                        <div
-                          className="card_block gap-4 mb-4 "
-                          style={{ display: "flex" }}
-                        >
-                          {this.state.dummy.map((test) =>
-                            test.batch === obj ? (
-                              <Cards person={test} key={test.id} />
-                            ) : obj == "Before 2018" && test.batch < 2018 ? (
-                              <Cards person={test} key={test.id} />
-                            ) : null
+                      <section className="container_outside_cards">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                          {this.state.dummy.map(
+                            (test) =>
+                              test.batch < 2018 && (
+                                <Cards person={test} key={test.id} />
+                              )
                           )}
                         </div>
                       </section>
                     </div>
-                  </>
-                ) : obj != this.state.selected_year ? (
-                  <div
-                    className="hide"
-                    id={obj}
-                    style={{ display: "flex", margin: "0 8vw 10px 8vw" }}
-                    style={{ display: "none" }}
-                  >
-                    <section className="container_outside_cards">
-                      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                        {this.state.dummy.map(
-                          (test) =>
-                            test.batch < 2018 && (
-                              <Cards person={test} key={test.id} />
-                            )
-                        )}
-                      </div>
-                    </section>
-                  </div>
-                ) : this.state.selected_year == "Before 2018" ? (
-                  <div
-                    className="hide"
-                    id={obj}
-                    style={{ display: "flex", margin: "0 4vw 10px 4vw" }}
-                  >
-                    <section
-                      className="container_outside_cards"
-                      style={checking}
+                  ) : this.state.selected_year == "Before 2018" ? (
+                    <div
+                      className="hide"
+                      id={obj}
+                      style={{ display: "flex", margin: "0 4vw 10px 4vw" }}
                     >
-                      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                        {this.state.dummy.map(
-                          (test) =>
-                            test.batch < 2018 && (
-                              <Cards person={test} key={test.id} />
-                            )
-                        )}
-                      </div>
-                    </section>{" "}
-                  </div>
-                ) : (
-                  <div
-                    className="hide"
-                    id={obj}
-                    style={{ display: "flex", margin: "0 4vw 10px 4vw" }}
-                  >
-                    <section
-                      className="container_outside_cards"
-                      style={checking}
+                      <section
+                        className="container_outside_cards"
+                        style={checking}
+                      >
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                          {this.state.dummy.map(
+                            (test) =>
+                              test.batch < 2018 && (
+                                <Cards person={test} key={test.id} />
+                              )
+                          )}
+                        </div>
+                      </section>{" "}
+                    </div>
+                  ) : (
+                    <div
+                      className="hide"
+                      id={obj}
+                      style={{ display: "flex", margin: "0 4vw 10px 4vw" }}
                     >
-                      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
-                        {this.state.dummy.map(
-                          (test) =>
-                            test.batch === obj && (
-                              <Cards person={test} key={test.id} />
-                            )
-                        )}
-                      </div>
-                    </section>
-                  </div>
-                )}
-              </React.Fragment>
-            ))}
+                      <section
+                        className="container_outside_cards"
+                        style={checking}
+                      >
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                          {this.state.dummy.map(
+                            (test) =>
+                              test.batch === obj && (
+                                <Cards person={test} key={test.id} />
+                              )
+                          )}
+                        </div>
+                      </section>
+                    </div>
+                  )}
+                </React.Fragment>
+              ))}
+            </div>
           </div>
-        </div>
-      </Layout>
-    );
+        </Layout>
+      );
+    }
   }
 }
 export default Alumni;
