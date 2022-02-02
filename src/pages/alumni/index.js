d8bb1dab2a9a81e5a35b02b3eb84509fdd735e95
Avishrant Sharma <avishrants@gmail.com>
2022-02-02 15:58:56 +0530

    Port `alumni` page to boilerplate code
    Signed-off-by: Avishrant Sharma <avishrants@gmail.com>

--- src/pages/alumni/index.js
+++ src/pages/alumni/index.js
@@ -1,5 +1,5 @@
 // Components
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Layout from "components/Layout/Layout";
 import Loader from "react-loader-spinner";
@@ -81,119 +81,171 @@ const AlumniSwiper = (props) => {
   );
 };
 
-const Alumni = () => {
-  const [alumni, setData] = useState({
-    wait: true,
-    data: [],
-    selected_year: 0,
-  });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios(alumniURL);
-      setData(() => ({ ...alumni, data: response.data, wait: false }));
-    };
-    fetchData();
-  });
-
+// <------------- Funtional Component ------------->
+const MainBody = ({ data }) => {
+  const [selected_year, setYear] = useState("0");
   const year_of_grad_options = [2021, 2020, 2019, "Before 2019"];
-  const select_year = (e) => {
-    setData({ ...alumni, selected_year: e.target.value });
-  };
+  const select_year = (e) => setYear(e.target.value);
   return (
-    <>
-      <Head title="Alumni" />
-      <Layout>
-        {alumni.wait ? (
-          <div className="flex h-90v justify-center items-center">
-            <Loader
-              type="Puff"
-              color="#00BFFF"
-              height={100}
-              width={100}
-              timeout={100000} // 10 secs wait until error message shows
-            />
-          </div>
-        ) : (
-          <div className="lg:m-12 lg:p-8">
-            {/* Filter for selecting year to view */}
-            <select
-              className="h-10 text-white bg-zinc-800 rounded-lg p-2 font-monty"
-              onChange={select_year}
-            >
-              <option value={0}>All Year</option>
-              {year_of_grad_options.map((year) => (
-                <option value={year} key={year}>
-                  {year}
-                </option>
-              ))}
-            </select>
+    <React.Fragment>
+      <div className="lg:m-12 lg:p-8">
+        {/* Filter for selecting year to view */}
+        <select
+          className="h-10 text-white bg-zinc-800 rounded-lg p-2 font-monty"
+          onChange={select_year}
+        >
+          <option value={0}>All Year</option>
+          {year_of_grad_options.map((year) => (
+            <option value={year} key={year}>
+              {year}
+            </option>
+          ))}
+        </select>
 
-            <div className="px-8">
-              {year_of_grad_options.map((year, index) => (
+        <div className="px-8">
+          {year_of_grad_options.map((year, index) => (
+            <div
+              className="batch text-white text-4xl font-monty text-center transition-all"
+              key={year}
+            >
+              {year !== "Before 2019" ? (
+                // Title of Swiper Section
                 <div
-                  className="batch text-white text-4xl font-monty text-center transition-all"
-                  key={year}
+                  className={
+                    (String(year) === selected_year || selected_year === "0"
+                      ? "h-auto"
+                      : "h-0") + " transition-all overflow-hidden"
+                  }
                 >
-                  {year !== "Before 2019" ? (
-                    // Title of Swiper Section
-                    <div
-                      className={
-                        (year === alumni.selected_year ||
-                        alumni.selected_year === 0
-                          ? "h-auto"
-                          : "h-0") + " transition-all overflow-hidden"
-                      }
-                    >
-                      {" "}
-                      Batch
-                      <span style={{ color: "rgb(46, 224, 154)" }}>{year}</span>
-                      <AlumniSwiper>
-                        {alumni.data
-                          .filter((user) => user.batch === year)
-                          .map((user) => (
-                            <SwiperSlide
-                              className="flex justify-center"
-                              key={user.name}
-                            >
-                              <AlumniCard person={user} />
-                            </SwiperSlide>
-                          ))}
-                      </AlumniSwiper>
-                    </div>
-                  ) : (
-                    <div
-                      className={
-                        (year === alumni.selected_year ||
-                        alumni.selected_year === 0
-                          ? "h-auto"
-                          : "h-0") + " transition-all overflow-hidden"
-                      }
-                    >
-                      {" "}
-                      Batch before
-                      <span style={{ color: "rgb(46, 224, 154)" }}> 2019 </span>
-                      <AlumniSwiper>
-                        {alumni.data
-                          .filter((user) => user.batch < 2019)
-                          .map((user) => (
-                            <SwiperSlide
-                              className="flex justify-center"
-                              key={user.name}
-                            >
-                              <AlumniCard person={user} />
-                            </SwiperSlide>
-                          ))}
-                      </AlumniSwiper>
-                    </div>
-                  )}
+                  Batch{" "}
+                  <span style={{ color: "rgb(46, 224, 154)" }}>{year}</span>
+                  <AlumniSwiper>
+                    {data
+                      .filter((user) => user.batch === year)
+                      .map((user) => (
+                        <SwiperSlide
+                          className="flex justify-center"
+                          key={user.name}
+                        >
+                          <AlumniCard person={user} />
+                        </SwiperSlide>
+                      ))}
+                  </AlumniSwiper>
                 </div>
-              ))}
+              ) : (
+                <div
+                  className={
+                    (String(year) === selected_year || selected_year === "0"
+                      ? "h-auto"
+                      : "h-0") + " transition-all overflow-hidden"
+                  }
+                >
+                  {" "}
+                  Batch before
+                  <span style={{ color: "rgb(46, 224, 154)" }}> 2019 </span>
+                  <AlumniSwiper>
+                    {data
+                      .filter((user) => user.batch < 2019)
+                      .map((user) => (
+                        <SwiperSlide
+                          className="flex justify-center"
+                          key={user.name}
+                        >
+                          <AlumniCard person={user} />
+                        </SwiperSlide>
+                      ))}
+                  </AlumniSwiper>
+                </div>
+              )}
             </div>
-          </div>
-        )}
-      </Layout>
-    </>
+          ))}
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
+
+class Alumni extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      wait: true,
+      data: [],
+      error: false,
+      errorData: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(alumniURL, { timeout: 10000 })
+      .then((response) => {
+        this.setState({ data: response.data, wait: false });
+      })
+      .catch((err) => {
+        console.log(err.response);
+        let msg = "Request Timed Out";
+        if (err.response) {
+          msg =
+            err.response.status && err.response.statusText
+              ? "API Error: " +
+                err.response.status +
+                " " +
+                err.response.statusText
+              : "API request failed";
+        }
+        this.setState({ error: true, errorMsg: msg, wait: false });
+      });
+  }
+
+  renderLoader() {
+    if (this.state.wait) {
+      return (
+        <div className="flex h-90v justify-center items-center">
+          <Loader
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={10000} // 10 secs wait until error message shows
+          />
+        </div>
+      );
+    }
+  }
+
+  renderError() {
+    if (this.state.error) {
+      return (
+        <div className="flex flex-wrap h-90v">
+          <div className="flex items-end md:items-center justify-end w-full md:w-1/2">
+            <picture className="flex justify-center md:justify-end px-8">
+              <source srcSet="error.webp" type="image/webp" />
+              <img src="error.webp" className="w-1/2" alt="error_image" />
+            </picture>
+          </div>
+          <div className="flex flex-col items-center justify-center w-full md:w-1/2 text-white p-4 text-4xl">
+            <p className="w-full text-red-500">{this.state.errorMsg}</p>
+            <p className="w-full text-xl">
+              Ah Snap! Something was broken. We're trying to fix this
+            </p>
+          </div>
+        </div>
+      );
+    }
+  }
+  render() {
+    return (
+      <Layout>
+        <Head title="PAGE TITLE GOES HERE" />
+        {this.renderLoader()}
+        {this.renderError()}
+        {this.state.wait || this.state.error ? (
+          ""
+        ) : (
+          <MainBody data={this.state.data} />
+        )}
+      </Layout>
+    );
+  }
+}
 export default Alumni;
