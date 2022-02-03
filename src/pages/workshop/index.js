0d5e3c169077133be0c4c9e0f810a55abf5241ca
baamaboi <samarjiitsingh@gmail.com>
2022-02-03 12:19:25 +0530

    Workshop Layout

--- src/pages/workshop/index.js
+++ src/pages/workshop/index.js
@@ -1,256 +1,218 @@
 // Components
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState, useRef } from "react";
+import GenericPage from "pageBoiler";
 import Layout from "components/Layout/Layout";
-import axios from "axios";
-import Loader from "react-loader-spinner";
 import Head from "utils/helmet";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay, Pagination } from "swiper";
 
 // Icons and Styles
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFilter,
+  faCalendarAlt,
+  faMapMarkerAlt,
+  faTimes
+} from "@fortawesome/free-solid-svg-icons";
 import "./workshopcard.css";
 
 // Constants, JSONs, and Styles
 import { worskhopURL } from "utils/routes";
+import { data } from "autoprefixer";
+import "swiper/css";
+import "swiper/css/scrollbar";
+
+// Drop Down Filter List
+const MediaIcons = (props) => {
+  return <React.Fragment></React.Fragment>;
+};
 
 const WorkshopCard = ({ shop }) => {
-  console.log(shop);
-
-  const [showModal, setShowModal] = useState(false);
+  let [toggle, setToggle] = useState(true);
+  const ref = useRef();
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      if (!toggle && ref.current && !ref.current.contains(e.target)) {
+        setToggle(true);
+      }
+    };
+    document.addEventListener("mousedown", checkIfClickedOutside);
+    return () => {
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [toggle]);
   return (
-    <>
-      <section className="workshop-card-section">
-        <div
-          className="workshop-card relative flex items-end overflow-hidden p-4 text-center border-black-700 border-2 rounded-lg "
-          style={{ backgroundImage: `url("${shop.cover_webp_url}")` }} //./images/spec_workshop11.jpg
-        >
-          <div className="workshop-content relative flex flex-col items-center w-full p-4">
-            <h2 className="workshop-title text-xl font-bold text-white">
-              {shop.title}
-            </h2>
-            <p className="copy text-lg italic text-white"> At {shop.venue}</p>
-            <button
-              id="open-btn"
-              onClick={() => setShowModal(true)}
-              className="workshop-btn cursor-pointer mt-6 text-xs font-bold uppercase text-black bg-white rounded-sm hover:bg-green-400"
-            >
-              Read More
-            </button>
-          </div>
+    <React.Fragment>
+    <div className={"contain z-40" + (toggle ? " hidden" : " block")}>
+      <div className="z-50 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white text-black text-justify w-1/2 h-1/2 p-4" ref={ref}>
+        <div className="flex w-full justify-end">
+          <button onClick={() => {setToggle((prevState) => !prevState);}}>
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
         </div>
-        {showModal ? (
-          <>
-            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-              <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                <div className="rounded-lg relative flex flex-col w-full shadow backdrop-filter backdrop-blur-xl outline-none focus:outline-none border-2 border-teal-400">
-                  <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t text-center">
-                    <h3 className="text-3xl font-semibold text-white">
-                      {shop.title}
-                    </h3>
-                    <button
-                      className="p-1 ml-auto bg-transparent border-0 text-white opacity-2 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                      onClick={() => setShowModal(false)}
-                    >
-                      <span className="bg-transparent text-white hover:text-green-500 opacity-2 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                        ×
-                      </span>
-                    </button>
-                  </div>
-                  <div className="relative p-6 flex-auto">
-                    <p className="my-4 text-white text-lg leading-relaxed">
-                      {shop.description}
-                    </p>
-                  </div>
-                  <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                    <button
-                      className="text-white hover:text-black hover:bg-green-400 rounded-sm background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={() => setShowModal(false)}
-                    >
-                      Close
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-          </>
-        ) : null}
-      </section>
-    </>
+        {shop.description}
+      </div>
+    </div>
+    <div className="workshop-card-container border-4">
+      <div className="">
+        <picture className="object-cover max-h-full w-full">
+          <source srcSet={shop.cover_webp_url} type="image/webp" />
+          <img src={shop.cover_url} alt={shop.title + "_pic"} />
+        </picture>
+      </div>
+      <div className="z-10 flex flex-wrap text-white card-overlay w-full h-full justify-center pt-8">
+        <span className="block self-start text-center text-2xl">
+          {shop.title}
+        </span>
+        <span className="flex flex-wrap w-full text-center self-center">
+          <a className="w-full p-4 pointer-events-none" href="#" target="_blank" rel="noopener noreferrer">Register Now</a>
+          <button className="w-full p-4"  onClick={() => {setToggle((prevState) => !prevState);}}>Read More</button>
+        </span>
+        <span className="flex flex-wrap w-full text-right self-end pb-4 pr-2">
+          <span className="block w-full">
+            <FontAwesomeIcon icon={faCalendarAlt} />
+            {" " + shop.event_date.toDateString()}
+          </span>
+          <span className="block w-full">
+            <FontAwesomeIcon icon={faMapMarkerAlt} />
+            {" " + shop.venue }
+          </span>
+        </span>
+      </div>
+    </div>
+    </React.Fragment>
   );
 };
 
-class MainBody extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: this.props.data,
-      selected_year: 0,
+const MainBody = (props) => {
+  let [displayData, setDisplayData] = useState(props.data);
+  let [toggle, setToggle] = useState(true);
+  let filter = [2021, 2020, 2019, 2018];
+  const ref = useRef();
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      if (!toggle && ref.current && !ref.current.contains(e.target)) {
+        setToggle(true);
+      }
     };
-  }
-
-  Selected_Year = (e) => {
-    this.setState({ selected_year: e.target.value });
-  };
-
-  datTimeHandler = (dat) => {
-    return new Date(dat).getFullYear();
-  };
-  render() {
-    const year_of_grad = [2021, 2020, 2019, 2018];
-    return (
-      <div className="">
-        <header className="head my-5">
-          <div className="text-5xl font-bold text-center my-16 sm:text-7xl md:text-8xl">
-            WORKSHOPS
-          </div>
-
-          <div className="flex flex-row flex-wrap justify-center">
-            <img
-              alt="arduino_pic"
-              className="sm:rounded-l-lg w-64 border-2 border-y-teal-400 border-l-teal-400"
-              src="./images/20944391.jpg"
-            />
-
-            <div className="border-y-teal-400 border-r-teal-400 border-2 w-64 p-6 text-center align-middle text-lg self-center text-monty shadow backdrop-filter backdrop-blur-lg section-content sm:rounded-r-lg lg:w-1/3 sm:w-1/2">
-              {" "}
-              SPEC provides students to keep a beady eye with the ever changing
-              technology by holding workshops on many fascinating topics. For
-              instance, many workshops on Arduino, Photoshop, IOT, Integrated
-              circuits are held which ignites the passion for electronics and
-              technology among students.
-            </div>
-          </div>
-        </header>
-        <div className="flex mt-5 mr-16 justify-center sm:justify-end">
-          <span className="mt-14 ml-2 mr-2 ">
-            <FontAwesomeIcon icon={faFilter} size="1x" />
-          </span>
-          <select
-            className="h-10 text-white font-bold bg-zinc-800  rounded-lg p-2 font-monty "
-            onChange={this.Selected_Year}
+    document.addEventListener("mousedown", checkIfClickedOutside);
+    return () => {
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [toggle]);
+  return (
+    <React.Fragment>
+      <div className="flex justify-end">
+        <div className="fixed md:relative right-0 top-0 md:mr-8 text-white z-20" ref={ref}>
+        <button
+            className="flex items-center justify-center h-16 w-16"
+            aria-label="Social Media"
+            onClick={() => {
+              setToggle((prevState) => !prevState);
+            }}
           >
-            <option value={0}>All Year</option>
-            {year_of_grad.map((obj) => (
-              <option value={obj}>{obj}</option>
-            ))}
-          </select>
-        </div>
-        <div className="mx-auto" style={{ padding: "auto" }}>
-          {year_of_grad.map((obj) => (
-            <React.Fragment key={obj}>
-              {this.state.selected_year === 0 ? (
-                <div>
-                  <div className="text-white  text-center text-4xl py-8 font-outfit">
-                    YEAR{" "}
-                    <span style={{ color: "rgb(46, 224, 154)" }}>{obj}</span>
-                  </div>
-                  <div className="workshop-page-content grid p-4">
-                    {this.state.data.map(
-                      (element) =>
-                        this.datTimeHandler(element.event_date) === obj && (
-                          <WorkshopCard key={element.title} shop={element} />
-                        )
-                    )}
-                  </div>
-                </div>
-              ) : obj === this.state.selected_year ? (
-                <div>
-                  <div
-                    className="text-white text-center text-4xl py-8 font-outfit"
-                    style={{ fontSize: "40px" }}
+            <FontAwesomeIcon className="text-2xl" icon={faFilter} />
+          </button>
+          <div
+            className={
+              "relative h-full w-full" +
+              (toggle ? " hidden" : " block")
+            }
+          >
+              <ul className="absolute bg-gray-600 right-8 list-none list-outside w-max">
+              <li
+                className="p-4 text-center"
+              >
+                <a
+                  className="bg-transparent hover:bg-blue-700 text-white font-bold text-lg text-center"
+                  aria-label="Filter for show all"
+                  href="#"
+                  data-filter="All Years"
+                  onClick={(e) => {
+                    setToggle((prevState) => !prevState);
+                    setDisplayData(() => props.data);
+                  }}
+                >
+                  All Years
+                </a>
+              </li>
+              {filter.map((year, index) => (
+                <li
+                  className="p-4 text-center"
+                  key={index}
+                >
+                  <a
+                    className="bg-transparent hover:bg-blue-700 text-white font-bold text-lg"
+                    aria-label={`Filter for Year ${year}`}
+                    href="#"
+                    data-filter={year}
+                    onClick={(e) => {
+                      setToggle((prevState) => !prevState);
+                      setDisplayData(() => {
+                        return props.data.filter(
+                          (element) =>
+                            element.event_date.getFullYear() ==
+                            e.target.getAttribute("data-filter")
+                        );
+                      });
+                    }}
                   >
-                    YEAR{" "}
-                    <span style={{ color: "rgb(46, 224, 154)" }}>{obj}</span>
-                  </div>
-                  <div className="workshop-page-content grid p-4">
-                    {this.state.data.map(
-                      (element) =>
-                        this.datTimeHandler(element.event_date) === obj && (
-                          <WorkshopCard key={element.title} shop={element} />
-                        )
-                    )}
-                  </div>
-                </div>
-              ) : null}
-            </React.Fragment>
-          ))}
+                    {year}
+                  </a>
+                </li>
+              ))}
+              </ul>
+          </div>
         </div>
       </div>
-    );
-  }
-}
+      <Swiper
+      className="alumni-swiper"
+      modules={[Pagination, Navigation, Autoplay]}
+      pagination={{ dynamicBullets: true, clickable: true }}
+      navigation={true}
+      // loop={true}
+      // autoplay={{ delay: 100, disableOnInteraction: false }}
+      loopAdditionalSlides={30}
+      breakpoints={{
+        640: { slidesPerView: 2 },
+        1024: { slidesPerView: 3 },
+        1280: { slidesPerView: 4 },
+      }}
+    >
+     {displayData.map((shop) => {
+          return (
+            <SwiperSlide>
+              <WorkshopCard shop={shop} key={shop.id} />
+              </SwiperSlide>
+          );
+        })} 
+    </Swiper>
+      {/* <div className="flex flex-wrap">
+        {displayData.map((shop) => {
+          return (
+            <div className="flex w-full md:w-1/2 lg:w-1/4 xl:w-1/6 p-4">
+              <WorkshopCard shop={shop} key={shop.id} />
+            </div>
+          );
+        })}
+      </div> */}
+    </React.Fragment>
+  );
+};
 
-class Workshop extends Component {
+class Workshop extends GenericPage {
   constructor() {
     super();
-    this.state = {
-      wait: true,
-      data: [],
-      error: false,
-      errorMsg: [],
-    };
-  }
-  componentDidMount() {
-    axios
-      .get(worskhopURL + "awda", { timeout: 10000 })
-      .then((response) => {
-        this.setState({ data: response.data, wait: false });
-      })
-      .catch((err) => {
-        console.log(err.response);
-        let msg = "Request Timed Out";
-        if (err.response) {
-          msg =
-            err.response.status && err.response.statusText
-              ? "API Error: " +
-                err.response.status +
-                " " +
-                err.response.statusText
-              : "API request failed";
-        }
-        this.setState({ error: true, errorMsg: msg, wait: false });
-      });
+    this.state.url = worskhopURL;
   }
 
-  renderLoader() {
-    if (this.state.wait) {
-      return (
-        <div className="flex h-90v justify-center items-center">
-          <Loader
-            type="Puff"
-            color="#00BFFF"
-            height={100}
-            width={100}
-            timeout={10000} // 10 secs wait until error message shows
-          />
-        </div>
-      );
-    }
-  }
-
-  renderError() {
-    if (this.state.error) {
-      return (
-        <div className="flex flex-wrap h-90v">
-          <div className="flex items-end md:items-center justify-end w-full md:w-1/2">
-            <picture className="flex justify-center md:justify-end px-8">
-              <source srcSet="error.webp" type="image/webp" />
-              <img src="error.webp" className="w-1/2" alt="error_image" />
-            </picture>
-          </div>
-          <div className="flex flex-col items-center justify-center w-full md:w-1/2 text-white p-4 text-4xl">
-            <p className="w-full text-red-500">{this.state.errorMsg}</p>
-            <p className="w-full text-xl">
-              Ah Snap! Something was broken. We're trying to fix this
-            </p>
-          </div>
-        </div>
-      );
-    }
-  }
   render() {
+    let data = this.state.data;
+    data.forEach((element) => {
+      element.event_date = new Date(element.event_date);
+    });
+    const sortedData = data.sort((a, b) => a.event_date < b.event_date);
+    this.state.data = sortedData;
     return (
       <Layout>
         <Head title="workshop" />
